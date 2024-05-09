78c45cb644be7e49e146bfede493c6a5d5249aba
daylightcoderj6 <jananibharathi6@gmail.com>
2024-05-09 20:16:02 +0530

    added Navbar and NavsTabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import NavsTabs from './Components/NavsTabs'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar/>
+      <NavsTabs/>
+    </div>
   )
 }
 
---
+++ src/Components/NavsTabs.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './NavsTabs.css'
+
+const NavsTabs = () => {
+  return (
+    <div>
+      <ul className="nav nav-underline justify-content-center">
+  <li className="nav-item me-4">
+    <a className="nav-link active" aria-current="page" href="#">Home</a>
+  </li>
+  <li className="nav-item me-4">
+    <a className="nav-link cars" href="#">Cars</a>
+  </li>
+  <li className="nav-item me-4">
+    <a className="nav-link about" href="#">About Us</a>
+  </li>
+  <li className="nav-item">
+    <p className="nav-link contact" href="#">Contact Us</p>
+  </li>
+</ul>
+    </div>
+  )
+}
+
+export default NavsTabs
