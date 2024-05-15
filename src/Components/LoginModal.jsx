12dee4f6a244dbc9b52043c15c67538650bd2a08
daylightcoderj6 <jananibharathi6@gmail.com>
2024-05-15 07:28:01 +0530

    correcetd form validation in Login and Registration Modals

--- src/Components/LoginModal.jsx
+++ src/Components/LoginModal.jsx
@@ -1,114 +1,130 @@
 import React, { useState } from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
-import './LoginModal.css'
-
+import "./LoginModal.css";
+import axios from "axios";
 
 const LoginModal = () => {
-    const [formState , setFormState] = useState({
-        username : "" ,
-        password : "" ,
-    })
-    
-    const handleUsername = (event) => {
-      setFormState({
-        ...formState , 
-        username : event.target.value , 
-      })
-    }
-   const handlePassword = (event) => {
-    setFormState({
-      ...formState , 
-      password : event.target.value
-    })
-   }
+  
+  const handleSubmit = (values) => {
+    axios.post("http://localhost:4000/login", {
+      values,
+    });
+    console.log(values);
+  };
 
-    const handleCancel = () => {
-     setFormState({
-      username : "" ,
-      password : "" ,
-     })
-    }
+  const handleCancel = (formik) => {
+    formik.resetForm();
+  };
+
   return (
     <div>
-         <div
-                className="modal fade"
-                id="staticBackdrop"
-                data-bs-backdrop="static"
-                data-bs-keyboard="false"
-                tabIndex="-1"
-                aria-labelledby="staticBackdropLabel"
-                aria-hidden="true"
+      <div
+        className="modal fade"
+        id="staticBackdrop"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabIndex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="staticBackdropLabel">
+                Enter Username and Password
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="form formFormik">
+              <Formik
+                initialValues={{ username: "", password: "" }}
+                validate={(values) => {
+                  const errors = {};
+                  if (!values.username) {
+                    errors.username = "*Username is required";
+                  }
+                  if (!values.password) {
+                    errors.password = "*Password is required";
+                  }
+                  return errors;
+                }}
+                onSubmit={handleSubmit}
               >
-                <div className="modal-dialog">
-                  <div className="modal-content">
-                    <div className="modal-header">
-                      <h1 className="modal-title fs-5" id="staticBackdropLabel">
-                        Enter Username and Password
-                      </h1>
+                {(formik) => (
+                  <Form>
+                    <div className="mb-2">
+                      <label
+                        htmlFor="formGroupExampleInput"
+                        className="form-label UserPass"
+                      >
+                        Username
+                      </label>
+                      <Field
+                        id="usernameInput"
+                        name="username"
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter Username"
+                      />
+                      <ErrorMessage
+                        name="username"
+                        component="span"
+                        className="error"
+                      />
+                    </div>
+
+                    <div>
+                      <label
+                        htmlFor="formGroupExampleInput"
+                        className="form-label UserPass"
+                      >
+                        Password
+                      </label>
+                      <Field
+                        id="passwordInput"
+                        name="password"
+                        type="password"
+                        className="form-control"
+                        placeholder="Enter Password"
+                      />
+                      <ErrorMessage
+                        name="password"
+                        component="span"
+                        className="error"
+                      />
+                    </div>
+
+                    <div className="buttons mt-4">
+                      <button
+                        type="submit"
+                        className="btn btn-success"
+                        data-bs-dismiss="modal"
+                      >
+                        Submit
+                      </button>
+
                       <button
                         type="button"
-                        className="btn-close"
-                        data-bs-dismiss="modal"
-                        aria-label="Close"
-                      ></button>
+                        className="btn btn-secondary ms-2"
+                        onClick={() => handleCancel(formik)}
+                      >
+                        Cancel
+                      </button>
                     </div>
-                    <div className="form formFormik">
-      <Formik
-        initialValues={{ username: "", password: "" }}
-        validate={(values) => {
-          const errors = {};
-          if (!values.username) {
-            errors.username = "*Username is required";
-          }
-          if (!values.password) {
-            errors.password = "*Password is required";
-          }
-          return errors;
-        }}
-        onSubmit={(values) => {
-          console.log(values);
-        }}
-      >
-        <Form>
-          <div className="mb-2"
-          onChange={handleUsername}
-                  >
-            <label htmlFor="formGroupExampleInput" className="form-label UserPass">Username</label>
-            <Field id="usernameInput" name="username" type="text" className="form-control" value={formState.username} placeholder="Enter Username"/>
-            <ErrorMessage name="username" component="span" className="error" />
+                  </Form>
+                )}
+              </Formik>
+            </div>
           </div>
-
-          <div 
-          onChange={handlePassword}
-        >
-            <label htmlFor="formGroupExampleInput" className="form-label UserPass">Password</label>
-            <Field id="passwordInput" name="password" type="password" className="form-control" value={formState.password} placeholder="Enter Password" />
-            <ErrorMessage name="password" component="span" className="error" />
-          </div>
-
-          <div className="buttons mt-4">
-            <button
-              type="submit"
-              className="btn btn-success"
-              data-bs-dismiss="modal"
-            >
-              Submit
-            </button>
-
-            <button type="button" className="btn btn-secondary ms-2" onClick={handleCancel}>
-              Cancel
-            </button>
-
-          </div>
-        </Form>
-      </Formik>
+        </div>
+      </div>
     </div>
-                  </div>
-     
-                </div>
-              </div>
-    </div>
-  )
-}
+  );
+};
 
-export default LoginModal
+export default LoginModal;
--- src/Components/RegistrationModal.jsx
+++ src/Components/RegistrationModal.jsx
@@ -1,116 +1,203 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
-import './RegistrationModal.css'
+import "./RegistrationModal.css";
+import axios from 'axios';
 
 const RegistrationModal = () => {
 
+  const handleSubmit = (values) => {
+    axios.post("http://localhost:4000/registration", {
+      values,
+    });
+   console.log(values)
+  }
+
+  const handleClear = (formik) => {
+    formik.resetForm();
+  }
+
   return (
     <div>
-         <div
-                className="modal fade"
-                id="staticBackdropRegister"
-                data-bs-backdrop="static"
-                data-bs-keyboard="false"
-                tabIndex="-1"
-                aria-labelledby="staticBackdropLabel"
-                aria-hidden="true"
-              >
-                <div className="modal-dialog">
-                  <div className="modal-content">
-                    <div className="modal-header">
-                      <h1 className="modal-title fs-5" id="staticBackdropLabel">
-                        Enter Registration Details
-                      </h1>
-                      <button
-                        type="button"
-                        className="btn-close"
-                        data-bs-dismiss="modal"
-                        aria-label="Close"
-                      ></button>
-                    </div>
-                    <div className="form formikForm">
-      <Formik
-        initialValues={{ username: "", password: "" , confirmPassword:"" , phoneNumber : "" , emailAddress : ""}}
-        validate={(values) => {
-          const errors = {};
-          if (!values.username) {
-            errors.username = "*Username is required";
-          }
-          if (!values.password) {
-            errors.password = "*Password is required";
-          }
-          if (!values.confirmPassword) {
-            errors.confirmPassword = "*Enter a valid password";
-          }
-          if (!values.phoneNumber) {
-            errors.phoneNumber = "*Enter Phone Number";
-          }
-          if (!values.emailAddress) {
-            errors.emailAddress = "*Enter Email Address";
-          }
-          return errors;
-        }}
-        onSubmit={(values) => {
-          console.log(values);
-        }}
+      <div
+        className="modal fade"
+        id="staticBackdropRegister"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabIndex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
       >
-        <Form className='mb-2'>
-          <div className="username mb-2" >
-            <label htmlFor="formGroupExampleInput" className="form-label inputsLabel">Enter Your Name</label>
-            <Field id="username" name="username" type="text"className="form-control" placeholder="Enter your Name"/>
-            <ErrorMessage name="username" component="span" className="error" />
-          </div>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="staticBackdropLabel">
+                Enter Registration Details
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="form formikForm">
+              <Formik
+                initialValues={{
+                  username: "",
+                  password: "",
+                  confirmPassword: "",
+                  phoneNumber: "",
+                  emailAddress: "",
+                }}
+                validate={(values) => {
+                  const errors = {};
+                  if (!values.username) {
+                    errors.username = "*Username is required";
+                  }
+                  if (!values.password) {
+                    errors.password = "*Password is required";
+                  }
+                  if (!values.confirmPassword) {
+                    errors.confirmPassword = "*Enter a valid password";
+                  }
+                  if (!values.phoneNumber) {
+                    errors.phoneNumber = "*Enter Phone Number";
+                  }
+                  if (!values.emailAddress) {
+                    errors.emailAddress = "*Enter Email Address";
+                  }
+                  return errors;
+                }}
+                onSubmit={handleSubmit}
+              >
+               {(formik) => (
+                 <Form>
+                 <div className="username mb-2">
+                   <label
+                     htmlFor="formGroupExampleInput"
+                     className="form-label inputsLabel"
+                   >
+                     Enter Your Name
+                   </label>
+                   <Field
+                     id="usernameInputRegister"
+                     name="username"
+                     type="text"
+                     className="form-control"
+                     placeholder="Enter your Name"
+                   />
+                   <ErrorMessage
+                     name="username"
+                     component="span"
+                     className="error"
+                   />
+                 </div>
 
-          <div className='mb-2' >
-            <label htmlFor="formGroupExampleInput" className="form-label inputsLabel">Password</label>
-            <Field id="password" name="password" type="password" className="form-control" 
-            placeholder="Enter Password" />
-            <ErrorMessage name="password" component="span" className="error" />
-          </div>
+                 <div className="mb-2" >
+                   <label
+                     htmlFor="formGroupExampleInput"
+                     className="form-label inputsLabel"
+                   >
+                     Password
+                   </label>
+                   <Field
+                     id="passwordInputRegister"
+                     name="password"
+                     type="password"
+                     className="form-control"
+                     placeholder="Enter Password"
+                   />
+                   <ErrorMessage
+                     name="password"
+                     component="span"
+                     className="error"
+                   />
+                 </div>
 
-          <div className='mb-2'>
-            <label htmlFor="formGroupExampleInput" className="form-label inputsLabel">Confirm Password</label>
-            <Field id="confirmPassword" name="confirmPassword" type="password" className="form-control"
-            placeholder="Confirm Password" />
-            <ErrorMessage name="confirmPassword" component="span" className="error" />
-          </div>
+                 <div className="mb-2">
+                   <label
+                     htmlFor="formGroupExampleInput"
+                     className="form-label inputsLabel"
+                   >
+                     Confirm Password
+                   </label>
+                   <Field
+                     id="confirmPasswordRegister"
+                     name="confirmPassword"
+                     type="password"
+                     className="form-control"
+                     placeholder="Confirm Password"
+                   />
+                   <ErrorMessage
+                     name="confirmPassword"
+                     component="span"
+                     className="error"
+                   />
+                 </div>
 
-          <div className='mb-2'>
-            <label htmlFor="formGroupExampleInput" className="form-label inputsLabel">Enter Phone Number</label>
-            <Field id="phoneNumber" name="phoneNumber" type="text" className="form-control"
-            placeholder="Enter Phone Number" />
-            <ErrorMessage name="phoneNumber" component="span" className="error" />
-          </div>
+                 <div className="mb-2">
+                   <label
+                     htmlFor="formGroupExampleInput"
+                     className="form-label inputsLabel"
+                   >
+                     Enter Phone Number
+                   </label>
+                   <Field
+                     id="phoneNumber"
+                     name="phoneNumber"
+                     type="text"
+                     className="form-control"
+                     placeholder="Enter Phone Number"
+                   />
+                   <ErrorMessage
+                     name="phoneNumber"
+                     component="span"
+                     className="error"
+                   />
+                 </div>
 
-          <div className='mb-4'>
-            <label htmlFor="formGroupExampleInput" className="form-label inputsLabel">Enter Email Address</label>
-            <Field id="emailAddress" name="emailAddress" type="email" className="form-control"
-            placeholder="Enter Email Address" />
-            <ErrorMessage name="emailAddress" component="span" className="error" />
-          </div>
+                 <div className="mb-4">
+                   <label
+                     htmlFor="formGroupExampleInput"
+                     className="form-label inputsLabel"
+                   >
+                     Enter Email Address
+                   </label>
+                   <Field
+                     id="emailAddress"
+                     name="emailAddress"
+                     type="email"
+                     className="form-control"
+                     placeholder="Enter Email Address"
+                   />
+                   <ErrorMessage
+                     name="emailAddress"
+                     component="span"
+                     className="error"
+                   />
+                 </div>
 
-          <div className="registerCancel">
-            <button
-              type="submit"
-              className="btn btn-success me-2"
-              data-bs-dismiss="modal"
-            >
-              Register
-            </button>
-            <button type="button" className="btn btn-secondary">
-              Cancel
-            </button>
+                 <div className="registerCancel">
+                   <button
+                     type="submit"
+                     className="btn btn-success me-2"
+                     data-bs-dismiss="modal"
+                   >
+                     Register
+                   </button>
+                   <button type="button" className="btn btn-secondary" onClick={() => handleClear(formik)}>
+                     Clear
+                   </button>
+                 </div>
+               </Form>
+               )}
+              </Formik>
+            </div>
           </div>
-        </Form>
-      </Formik>
+        </div>
+      </div>
     </div>
-                  </div>
-     
-                </div>
-              </div>
-    </div>
-    
-  )
-}
+  );
+};
 
-export default RegistrationModal
+export default RegistrationModal;
